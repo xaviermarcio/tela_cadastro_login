ce60f1e53e7f361ace31de73f0fff086f421b0de
Márcio Xavier <43189549+xaviermarcio@users.noreply.github.com>
2023-11-21 11:38:15 -0300

    Ajustes indentação

--- js/cadastro_validacao.js
+++ js/cadastro_validacao.js
@@ -6,87 +6,88 @@ const password = document.getElementById("password");
 const passwordConfirmation = document.getElementById("password-confirmation");
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    checkInputs();
+  checkInputs();
 });
 
-function checkInputs(){
-    const usernameValue = username.value;
-    const emailValue = email.value;
-    const emailConfirmationValue = emailConfirmation.value;
-    const passwordValue = password.value;
-    const passwordConfirmationValue = passwordConfirmation.value;
+function checkInputs() {
+  const usernameValue = username.value;
+  const emailValue = email.value;
+  const emailConfirmationValue = emailConfirmation.value;
+  const passwordValue = password.value;
+  const passwordConfirmationValue = passwordConfirmation.value;
 
-    if(usernameValue === ""){
-        setErrorFor(username, "O nome de usuário é obrigatório.");
-    }else if(usernameValue.length < 3){
-        setErrorFor(username,"O nome do usuário precisar ter mais de 3 caracteres.")
-    }
-    else{
-        setSuccessFor(username);
-    }
+  if (usernameValue === "") {
+    setErrorFor(username, "O nome de usuário é obrigatório.");
+  } else if (usernameValue.length < 3) {
+    setErrorFor(
+      username,
+      "O nome do usuário precisar ter mais de 3 caracteres."
+    );
+  } else {
+    setSuccessFor(username);
+  }
 
-    if(emailValue === ""){
-        setErrorFor(email, "O email é obrigatório");
-    }else if(!checkEmail(emailValue)){
-        setErrorFor(email,"Por favor, insira um email válido.");
-    }else{
-        setSuccessFor(email)
-    }
+  if (emailValue === "") {
+    setErrorFor(email, "O email é obrigatório");
+  } else if (!checkEmail(emailValue)) {
+    setErrorFor(email, "Por favor, insira um email válido.");
+  } else {
+    setSuccessFor(email);
+  }
 
-    if(emailConfirmationValue === ""){
-        setErrorFor(emailConfirmation,"A confirmação de email é obrigatória.");
-    }else if(emailValue !== emailConfirmationValue){
-        setErrorFor(emailConfirmation,"Email não confere.");
-    }else{
-        setSuccessFor(emailConfirmation);
-    }
+  if (emailConfirmationValue === "") {
+    setErrorFor(emailConfirmation, "A confirmação de email é obrigatória.");
+  } else if (emailValue !== emailConfirmationValue) {
+    setErrorFor(emailConfirmation, "Email não confere.");
+  } else {
+    setSuccessFor(emailConfirmation);
+  }
 
-    if(passwordValue === ""){
-        setErrorFor(password,"A senha é obrigatória.");
-    }else if(passwordValue.length < 8){
-        setErrorFor(password,"A senha precisa ter no minímo 8 caracteres");
-    }else{
-        setSuccessFor(password);
-    }
+  if (passwordValue === "") {
+    setErrorFor(password, "A senha é obrigatória.");
+  } else if (passwordValue.length < 8) {
+    setErrorFor(password, "A senha precisa ter no minímo 8 caracteres");
+  } else {
+    setSuccessFor(password);
+  }
 
-    if(passwordConfirmationValue === ""){
-        setErrorFor(passwordConfirmation,"A confirmação da senha é obrigatória.");
-    }else if(passwordValue !== passwordConfirmationValue){
-        setErrorFor(passwordConfirmation,"A senhas não conferem.");
-    }else{
-        setSuccessFor(passwordConfirmation);
-    }
+  if (passwordConfirmationValue === "") {
+    setErrorFor(passwordConfirmation, "A confirmação da senha é obrigatória.");
+  } else if (passwordValue !== passwordConfirmationValue) {
+    setErrorFor(passwordConfirmation, "A senhas não conferem.");
+  } else {
+    setSuccessFor(passwordConfirmation);
+  }
 
-    const formControls = form.querySelectorAll(".form-control");
+  const formControls = form.querySelectorAll(".form-control");
 
-    const formIsValid = [...formControls].every((formControl) => {
-        return formControl.className === "form-control success";
-    });
+  const formIsValid = [...formControls].every((formControl) => {
+    return formControl.className === "form-control success";
+  });
 
-    if (formIsValid) {
+  if (formIsValid) {
     console.log("O formulário está 100% válido!");
-    }
+  }
 }
 
 function setErrorFor(input, message) {
-    const formControl = input.parentElement;
-    const small = formControl.querySelector("small");
+  const formControl = input.parentElement;
+  const small = formControl.querySelector("small");
 
-    small.innerText = message;
-    formControl.className = "form-control error";
+  small.innerText = message;
+  formControl.className = "form-control error";
 }
 
 function setSuccessFor(input) {
-    const formControl = input.parentElement;
+  const formControl = input.parentElement;
 
-    formControl.className = "form-control success";
+  formControl.className = "form-control success";
 }
 
 function checkEmail(email) {
-    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-      email
-    );
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
 }
-  
--- js/login_validacao.js
+++ js/login_validacao.js
@@ -2,48 +2,48 @@ const email = document.getElementById("email");
 const password = document.getElementById("password");
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    checkInputs();
+  checkInputs();
 });
 
-function  checkInputs() {
-    const emailValue = email.value;
-    const passwordValue = password.value;
+function checkInputs() {
+  const emailValue = email.value;
+  const passwordValue = password.value;
 
-    if(emailValue === ""){
-        setErrorFor(email,"O email é obrigatório para fazer login...");
-    }else if(!checkEmail(emailValue)){
-        setErrorFor(email,"Por favor, digite um email válido!");
-    }else{
-        setSuccessFor(email)
-    }
+  if (emailValue === "") {
+    setErrorFor(email, "O email é obrigatório para fazer login...");
+  } else if (!checkEmail(emailValue)) {
+    setErrorFor(email, "Por favor, digite um email válido!");
+  } else {
+    setSuccessFor(email);
+  }
 
-    if(passwordValue ===""){
-        setErrorFor(password, "A senha é obrigatória!")
-    }else if(passwordValue.length < 8){
-        setErrorFor(password, "Senha tem que ter no mínimo 8 caracteres!")
-    }else{
-        setSuccessFor(password);
-    }
+  if (passwordValue === "") {
+    setErrorFor(password, "A senha é obrigatória!");
+  } else if (passwordValue.length < 8) {
+    setErrorFor(password, "Senha tem que ter no mínimo 8 caracteres!");
+  } else {
+    setSuccessFor(password);
+  }
 }
 
 function setErrorFor(input, message) {
-    const formControl = input.parentElement;
-    const small = formControl.querySelector("small");
+  const formControl = input.parentElement;
+  const small = formControl.querySelector("small");
 
-    small.innerText = message;//mensagem de erro
-    formControl.className = "form-control error";
+  small.innerText = message; //mensagem de erro
+  formControl.className = "form-control error";
 }
 
-function setSuccessFor(input){
-    const formControl = input.parentElement;
+function setSuccessFor(input) {
+  const formControl = input.parentElement;
 
-    formControl.className = "form-control success";//mensagem de sucesso
+  formControl.className = "form-control success"; //mensagem de sucesso
 }
 
 function checkEmail(email) {
-    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-      email
-    );
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
 }
